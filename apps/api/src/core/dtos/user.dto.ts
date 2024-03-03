9c0f8917e0edbbd7eb1b2bffbbbd10a918db795e
WhyNeet <arc.software.x@gmail.com>
2024-03-03 21:34:06 +0300

    feat(api): add User entity and DTO

---
+++ apps/api/src/core/dtos/user.dto.ts
@@ -0,0 +1,11 @@
+export class UserDto {
+  email: string;
+
+  username: string;
+
+  name: string;
+
+  bio?: string;
+
+  avatarId?: string;
+}
---
+++ apps/api/src/core/entities/user.entity.ts
@@ -0,0 +1,13 @@
+export class User {
+  email: string;
+
+  username: string;
+
+  password: string;
+
+  name: string;
+
+  bio?: string;
+
+  avatarId?: string;
+}
