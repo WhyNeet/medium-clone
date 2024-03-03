9a770be6b15dad79297b1a1d581f98c67f837a97
WhyNeet <arc.software.x@gmail.com>
2024-03-03 17:48:56 +0300

    feat(api): add configuration

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
+import { ConfigurationModule } from "./configuration/configuration.module";
 
 @Module({
-  imports: [],
+  imports: [ConfigurationModule],
   controllers: [],
   providers: [],
 })
