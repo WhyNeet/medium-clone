95257839a27064ac2efc4b7300fa00793f18ad7e
WhyNeet <arc.software.x@gmail.com>
2024-03-04 21:01:53 +0300

    chore(api): add cookieParser and ApplicationFactory

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,9 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { AppModule } from "@/app.module";
+import { ApplicationFactory } from "@/application/application.factory";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await ApplicationFactory.create(AppModule);
+
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
