2d2a62feeefde96e987380db26bc5f976c7e24ea
WhyNeet <arc.software.x@gmail.com>
2024-03-05 22:24:06 +0300

    refactor(api): move User decorator into core/decorators

---
+++ apps/api/src/core/decorators/user.decorator.ts
@@ -0,0 +1,9 @@
+import { TokenUser } from "@/frameworks/auth-services/jwt/types/token-user.interface";
+import { ExecutionContext, createParamDecorator } from "@nestjs/common";
+
+export const User = createParamDecorator(
+  (_: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user as TokenUser;
+  },
+);
