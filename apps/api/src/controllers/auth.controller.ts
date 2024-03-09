a1dd47b634cb73ae966e3636a996450e36263a1f
WhyNeet <arc.software.x@gmail.com>
2024-03-09 21:56:29 +0300

    refactor(api): add interceptor to set auth cookies

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -1,11 +1,10 @@
 import { CreateUserDto, UserCredentialsDto } from "@/core/dtos/user.dto";
+import { AuthTokenInterceptor } from "@/features/auth/auth-token.interceptor";
 import { AuthService } from "@/features/auth/auth.service";
 import { AuthException } from "@/features/exception/exceptions/auth.exception";
 import { UserFactoryService } from "@/features/user/user-factory.service";
 import { UserRepositoryService } from "@/features/user/user-repository.service";
-import { TokenType } from "@/frameworks/auth-services/jwt/types/token-type.enum";
-import { Body, Controller, Post, Res } from "@nestjs/common";
-import type { Response } from "express";
+import { Body, Controller, Post, UseInterceptors } from "@nestjs/common";
 
 @Controller("/auth")
 export class AuthController {
@@ -15,11 +14,9 @@ export class AuthController {
     private userFactoryService: UserFactoryService,
   ) {}
 
+  @UseInterceptors(AuthTokenInterceptor)
   @Post("/login")
-  public async login(
-    @Body() userCredentialsDto: UserCredentialsDto,
-    @Res({ passthrough: true }) response: Response,
-  ) {
+  public async login(@Body() userCredentialsDto: UserCredentialsDto) {
     const user = await this.userRepositoryService.getUserByEmail(
       userCredentialsDto.email,
     );
@@ -37,9 +34,6 @@ export class AuthController {
       user.id,
     );
 
-    response.cookie(TokenType.AccessToken, accessToken);
-    response.cookie(TokenType.RefreshToken, refreshToken);
-
     return {
       user: this.userFactoryService.createDto(user),
       accessToken,
@@ -47,19 +41,14 @@ export class AuthController {
     };
   }
 
+  @UseInterceptors(AuthTokenInterceptor)
   @Post("/register")
-  public async register(
-    @Body() createUserDto: CreateUserDto,
-    @Res({ passthrough: true }) response: Response,
-  ) {
+  public async register(@Body() createUserDto: CreateUserDto) {
     const user = await this.userRepositoryService.createUser(createUserDto);
 
     const { accessToken, refreshToken } = await this.authService.issueTokenPair(
       user.id,
     );
-
-    response.cookie(TokenType.AccessToken, accessToken);
-    response.cookie(TokenType.RefreshToken, refreshToken);
 
     return {
       user: this.userFactoryService.createDto(user),
--- apps/api/src/controllers/token.controller.ts
+++ apps/api/src/controllers/token.controller.ts
@@ -1,31 +1,31 @@
 import { CreateApiTokenDto } from "@/core/dtos/api-token.dto";
+import { AuthTokenInterceptor } from "@/features/auth/auth-token.interceptor";
 import { AuthService } from "@/features/auth/auth.service";
 import { User } from "@/features/user/user.decorator";
 import { AccessTokenGuard } from "@/frameworks/auth-services/jwt/guards/access-token.guard";
 import { RefreshTokenGuard } from "@/frameworks/auth-services/jwt/guards/refresh-token.guard";
-import { TokenType } from "@/frameworks/auth-services/jwt/types/token-type.enum";
 import { TokenUser } from "@/frameworks/auth-services/jwt/types/token-user.interface";
-import { Body, Controller, Post, Res, UseGuards } from "@nestjs/common";
-import type { Response } from "express";
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from "@nestjs/common";
 
 @Controller("/token")
 export class TokenController {
   constructor(private authService: AuthService) {}
 
+  @UseInterceptors(AuthTokenInterceptor)
   @UseGuards(RefreshTokenGuard)
   @Post("/refresh")
-  public async refreshToken(
-    @User() user: TokenUser,
-    @Res({ passthrough: true }) response: Response,
-  ) {
+  public async refreshToken(@User() user: TokenUser) {
     const { accessToken, refreshToken } = await this.authService.issueTokenPair(
       user.id,
     );
 
     // TODO: token rotation
-
-    response.cookie(TokenType.AccessToken, accessToken);
-    response.cookie(TokenType.RefreshToken, refreshToken);
 
     return { accessToken, refreshToken };
   }
